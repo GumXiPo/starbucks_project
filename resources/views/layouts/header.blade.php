625939c3f174c1677732aaed62857b0462cf89cf
Huy Pham <tatsumin123456@gmail.com>
2024-11-06 10:28:21 +0700

    login css

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -36,10 +36,7 @@
             @else
                 <ul>
                     <li>
-                        <a href="{{ route('login') }}">Login</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('login') }}">Sign In</a>
                     </li>
                 </ul>
             @endif
